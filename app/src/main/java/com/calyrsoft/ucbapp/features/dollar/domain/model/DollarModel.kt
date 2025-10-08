01e752538e011d229e8a49f350d6b4fa19b39157
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-10-07 23:25:06 -0400

    termino dolar conectando con firebase

--- app/src/main/java/com/calyrsoft/ucbapp/features/dollar/domain/model/DollarModel.kt
+++ app/src/main/java/com/calyrsoft/ucbapp/features/dollar/domain/model/DollarModel.kt
@@ -1,4 +1,6 @@
 package com.calyrsoft.ucbapp.features.dollar.domain.model
 
-data class DollarModel(val dollarOfficial: String, val dollarParelelo: String) {
-}
+data class DollarModel(
+    var dollarOfficial: String? = null,
+    var dollarParallel: String? = null
+)
