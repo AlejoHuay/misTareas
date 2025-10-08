190a5603822b5b7e82feea18801aa5e949df399d
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-10-07 23:05:16 -0400

    avanzo dollar hasta local sin firebase y tambien avanzo notis push hasta antes de recibir el token desde dollar

--- app/src/main/java/com/calyrsoft/ucbapp/navigation/Screen.kt
+++ app/src/main/java/com/calyrsoft/ucbapp/navigation/Screen.kt
@@ -4,5 +4,8 @@ sealed class Screen(val route: String) {
     object Home: Screen("home")
     object Github: Screen("github")
     object Profile: Screen("profile")
+
+    object Dollar: Screen("dollar")
     
 }
+
